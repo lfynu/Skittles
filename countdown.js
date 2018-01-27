a7e81e4c93cf3180d06e5282ade1329ecb986b3f
lfynu <603435227@qq.com>
2018-01-27 19:26:59 +0800

    彩虹糖倒计时第一次提交

---
+++ countdown.js
@@ -0,0 +1,184 @@
+var WINDOW_WIDTH = 1024;
+var WINDOW_HEIGHT = 768;
+var RADIUS = 8;
+var MARGIN_TOP = 60;
+var MARGIN_LEFT = 30;
+
+const endTime = new Date();
+endTime.setTime(endTime.getTime()+3600*1000)
+var curShowTimeSeconds = 0
+
+var balls = [];
+const colors = ["#33B5E5","#0099CC","#AA66CC","#9933CC","#99CC00","#669900","#FFBB33","#FF8800","#FF4444","#CC0000"]
+
+window.onload = function(){
+    //屏幕自适应
+    WINDOW_WIDTH = document.body.clientWidth
+    WINDOW_HEIGHT = document.body.clientHeight
+
+    MARGIN_LEFT = Math.round(WINDOW_WIDTH /10);
+    RADIUS = Math.round(WINDOW_WIDTH * 4 / 5 / 108)-1
+
+    MARGIN_TOP = Math.round(WINDOW_HEIGHT /5);
+
+    var canvas = document.getElementById('canvas');
+    var context = canvas.getContext("2d");
+
+    canvas.width = WINDOW_WIDTH;
+    canvas.height = WINDOW_HEIGHT;
+
+    curShowTimeSeconds = getCurrentShowTimeSeconds();
+    //利用setInterval开始帧动画
+    // setInterval(
+    //     function(){
+    //         render( context );
+    //         update();
+    //     }
+    //     ,
+    //     50
+    // );
+    function action(){
+        render( context );
+        update();
+
+        requestAnimationFrame(action);
+    }
+    action();
+}
+
+function getCurrentShowTimeSeconds() {
+    var curTime = new Date();
+    var ret = endTime.getTime() - curTime.getTime();
+    ret = Math.round( ret/1000 )
+    // console.log(ret);
+    return ret >= 0 ? ret : 0;
+    
+}
+
+function update(){
+
+    var nextShowTimeSeconds = getCurrentShowTimeSeconds();
+   
+    var nextHours = parseInt( nextShowTimeSeconds / 3600);
+    var nextMinutes = parseInt( (nextShowTimeSeconds - nextHours * 3600)/60 )
+    var nextSeconds = nextShowTimeSeconds % 60
+
+    var curHours = parseInt( curShowTimeSeconds / 3600);
+    var curMinutes = parseInt( (curShowTimeSeconds - curHours * 3600)/60 )
+    var curSeconds = curShowTimeSeconds % 60
+
+    if( nextSeconds != curSeconds ){
+        if( parseInt(curHours/10) != parseInt(nextHours/10) ){
+            addBalls( MARGIN_LEFT + 0 , MARGIN_TOP , parseInt(curHours/10) );
+        }
+        if( parseInt(curHours%10) != parseInt(nextHours%10) ){
+            addBalls( MARGIN_LEFT + 15*(RADIUS+1) , MARGIN_TOP , parseInt(curHours/10) );
+        }
+
+        if( parseInt(curMinutes/10) != parseInt(nextMinutes/10) ){
+            addBalls( MARGIN_LEFT + 39*(RADIUS+1) , MARGIN_TOP , parseInt(curMinutes/10) );
+        }
+        if( parseInt(curMinutes%10) != parseInt(nextMinutes%10) ){
+            addBalls( MARGIN_LEFT + 54*(RADIUS+1) , MARGIN_TOP , parseInt(curMinutes%10) );
+        }
+
+        if( parseInt(curSeconds/10) != parseInt(nextSeconds/10) ){
+            addBalls( MARGIN_LEFT + 78*(RADIUS+1) , MARGIN_TOP , parseInt(curSeconds/10) );
+        }
+        if( parseInt(curSeconds%10) != parseInt(nextSeconds%10) ){
+            addBalls( MARGIN_LEFT + 93*(RADIUS+1) , MARGIN_TOP , parseInt(nextSeconds%10) );
+        }
+
+        curShowTimeSeconds = nextShowTimeSeconds;
+    }
+
+    updateBalls();
+
+    // console.log( balls.length)
+}
+//给生成的彩色小球x速度和y速度还有模拟的重力加速度g并加入了底部边界判断并给予摩擦系数并模拟空气阻力
+function updateBalls(){
+
+    for( var i = 0 ; i < balls.length ; i ++ ){
+
+        balls[i].x += balls[i].vx;
+        balls[i].y += balls[i].vy;
+        balls[i].vy += balls[i].g;
+
+        if( balls[i].y >= WINDOW_HEIGHT-RADIUS ){
+            balls[i].y = WINDOW_HEIGHT-RADIUS;
+            balls[i].vy = - balls[i].vy*0.75;
+        }
+    }
+    //针对彩色小球数组中无限增加的优化
+    var cnt = 0
+    for( var i = 0 ; i < balls.length ; i ++ )
+        if( balls[i].x + RADIUS > 0 && balls[i].x -RADIUS < WINDOW_WIDTH )
+            balls[cnt++] = balls[i]
+
+    while( balls.length > cnt ){
+        balls.pop();
+    }
+}
+//当数字发生改变时，从数字矩阵数组上生成掉落彩色小球的对象函数
+function addBalls( x , y , num ){
+
+    for( var i = 0  ; i < digit[num].length ; i ++ )
+        for( var j = 0  ; j < digit[num][i].length ; j ++ )
+            if( digit[num][i][j] == 1 ){
+                var aBall = {
+                    x:x+j*2*(RADIUS+1)+(RADIUS+1),
+                    y:y+i*2*(RADIUS+1)+(RADIUS+1),
+                    g:1.5+Math.random(),
+                    vx:Math.pow( -1 , Math.ceil( Math.random()*1000 ) ) * 4,
+                    vy:-5,
+                    color: colors[ Math.floor( Math.random()*colors.length ) ]
+                }
+                //将生成的对象加入之前声明的数组
+                balls.push( aBall )
+            }
+}
+// 倒计时生成，绘制整个动画的函数
+function render( cxt ){
+
+    cxt.clearRect(0,0,WINDOW_WIDTH, WINDOW_HEIGHT);
+    //获取各个时间的具体时间
+    var hours = parseInt( curShowTimeSeconds / 3600);
+    var minutes = parseInt( (curShowTimeSeconds - hours * 3600)/60 )
+    var seconds = curShowTimeSeconds % 60
+
+    renderDigit( MARGIN_LEFT , MARGIN_TOP , parseInt(hours/10) , cxt )
+    renderDigit( MARGIN_LEFT + 15*(RADIUS+1) , MARGIN_TOP , parseInt(hours%10) , cxt )
+    renderDigit( MARGIN_LEFT + 30*(RADIUS + 1) , MARGIN_TOP , 10 , cxt )
+    renderDigit( MARGIN_LEFT + 39*(RADIUS+1) , MARGIN_TOP , parseInt(minutes/10) , cxt);
+    renderDigit( MARGIN_LEFT + 54*(RADIUS+1) , MARGIN_TOP , parseInt(minutes%10) , cxt);
+    renderDigit( MARGIN_LEFT + 69*(RADIUS+1) , MARGIN_TOP , 10 , cxt);
+    renderDigit( MARGIN_LEFT + 78*(RADIUS+1) , MARGIN_TOP , parseInt(seconds/10) , cxt);
+    renderDigit( MARGIN_LEFT + 93*(RADIUS+1) , MARGIN_TOP , parseInt(seconds%10) , cxt);
+    //绘制彩色小球数组中的小球
+    for( var i = 0 ; i < balls.length ; i ++ ){
+        cxt.fillStyle=balls[i].color;
+
+        cxt.beginPath();
+        cxt.arc( balls[i].x , balls[i].y , RADIUS , 0 , 2*Math.PI , true );
+        cxt.closePath();
+
+        cxt.fill();
+    }
+}
+//小球绘制数字的函数
+function renderDigit( x , y , num , cxt ){
+
+    cxt.fillStyle = "rgb(0,102,153)";
+
+    for( var i = 0 ; i < digit[num].length ; i ++ )
+        for(var j = 0 ; j < digit[num][i].length ; j ++ )
+            if( digit[num][i][j] == 1 ){
+                cxt.beginPath();
+                cxt.arc( x+j*2*(RADIUS+1)+(RADIUS+1) , y+i*2*(RADIUS+1)+(RADIUS+1) , RADIUS , 0 , 2*Math.PI )
+                cxt.closePath()
+
+                cxt.fill()
+            }
+}
+
